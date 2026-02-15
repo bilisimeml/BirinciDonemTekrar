a00cef21236b3b25eb5e322e6e713a6473b2984a
Akif Korkmaz <akifkorkmaztr@gmail.com>
2026-02-15 19:25:27 +0300

    MainActivity.kt edittext kontrolü

--- app/src/main/java/com/example/birincidonemtekrar/MainActivity.kt
+++ app/src/main/java/com/example/birincidonemtekrar/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.birincidonemtekrar
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -36,6 +37,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun ikinciAktiviteyeGit(view: View) {
+        if (binding.etText.text.toString() == "") {
+            binding.etText.error = "Lütfen bir isim giriniz"
+            Toast.makeText(
+                this,
+                "Lütfen bir isim giriniz",
+                Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
         val intent = Intent(this, SecondActivity::class.java)
         intent.putExtra("isim", binding.etText.text.toString())
         startActivity(intent)
