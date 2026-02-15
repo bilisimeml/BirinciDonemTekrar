92ad595709b920ffff3f4639cf4cc301ebb413bd
Akif Korkmaz <akifkorkmaztr@gmail.com>
2026-02-15 19:08:56 +0300

    ikinci aktivite tasarımı ve viewbinding

--- app/src/main/java/com/example/birincidonemtekrar/SecondActivity.kt
+++ app/src/main/java/com/example/birincidonemtekrar/SecondActivity.kt
@@ -5,12 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.birincidonemtekrar.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_second)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
